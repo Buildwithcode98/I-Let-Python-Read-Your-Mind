f3082f2ff2e96a00bb7d22f7a7c6c645b0d24bd3
BuildWithCode <devadattakamath3@gmail.com>
2025-12-19 13:02:15 +0530

    emoji to text

---
+++ emoji to text.py
@@ -0,0 +1,17 @@
+import demoji
+
+text = """I’ll be your silent supporter this year 💪.
+The journey you’re on is about to level up 🔥 🚀.
+A win 🏆 😤 is closer than you think.
+Till then, stay locked in 👀 🧠 ⚡."""
+
+print(demoji.findall(text))
+
+
+
+
+
+
+
+
+
